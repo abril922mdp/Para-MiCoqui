bd1f93936d31efa47a5804917eec80e218cd64a5
abril922mdp <abril922.mdp@gmail.com>
2026-07-21 19:54:13 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function abrirCarta(sobre){
   sobre.classList.add("abierto");
   document.getElementById("carta").classList.add("abierta");
 }
+
+function reproducirMusica(){
+  let musica = document.getElementById("musica");
+
+  if(musica.paused){
+    musica.play();
+  }else{
+    musica.pause();
+  }
+}
